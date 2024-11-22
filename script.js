4f9c1be598bfd47bf4f2b1802c2b62234e294492
FRJ74 <frankr74@gmail.com>
2024-11-21 20:59:33 -0600

    Update the display with the sorted numbers.

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Declare variables */
+
+const sortButton = document.getElementById("sort");
+
+const sortInputArray = (event) => {
+    event.preventDefault();
+    const inputValues = [...document.getElementsByClassName("values-dropdown")].map(dropdown => Number(dropdown.value));
+}
+
+sortButton.addEventListener("click", sortInputArray );
