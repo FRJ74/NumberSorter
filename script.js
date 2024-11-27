f0c8b55c54af25864f112141e4e835eea151ff40
FRJ74 <frankr74@gmail.com>
2024-11-27 17:09:42 -0600

    Use the forEach() method, and pass it an empty callback which takes num and i as the parameters.

--- script.js
+++ script.js
@@ -3,8 +3,15 @@
 const sortButton = document.getElementById("sort");
 
 const sortInputArray = (event) => {
-    event.preventDefault();
-    const inputValues = [...document.getElementsByClassName("values-dropdown")].map(dropdown => Number(dropdown.value));
+  event.preventDefault();
+
+  const inputValues = [
+    ...document.getElementsByClassName("values-dropdown")
+  ].map((dropdown) => Number(dropdown.value));
 }
 
-sortButton.addEventListener("click", sortInputArray );
+const updateUI = (array = []) => {
+    array.forEach((num, i) => {})
+  }
+
+  sortButton.addEventListener("click", sortInputArray);
