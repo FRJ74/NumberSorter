7c10d1aeb92499bea6550eba261b24fe29d580ec
FRJ74 <frankr74@gmail.com>
2024-12-02 16:28:17 -0600

    Use a temp variable to extract the value at array[i], then swap the values at array[i] and array[minIndex].

--- script.js
+++ script.js
@@ -38,8 +38,14 @@ const updateUI = (array = []) => {
     for (let i = 0; i < array.length; i++) {
       let minIndex = i;
     for (let j = i + 1; j < array.length; j++) {
-
+      console.log(array, array[j],array[minIndex]);
+      if (array[j] < array[minIndex]) {
+        minIndex = j;
+        }
       }
+      const temp = array[i];
+      array[i] = array[minIndex]; 
+      array[minIndex] = temp; 
     }
   };
 
